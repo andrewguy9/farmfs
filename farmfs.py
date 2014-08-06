1081efe0e5c6abe1fcc7f8eb9d8adfc067c6bbe8
Andrew Thomson <athomsonguy@gmail.com>
2014-08-05 21:53:14 -0700

    add blank exe file

---
+++ farmfs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser(description='Manage a farmfs instance.',)
+
+"""Builds a symlink farm out of a directory.
+Looks at a the directory and turns all the
+files into symlinks into the md5 sums of the files
+under .farmfs/data"""
+def main():
+  args = parser.parse_args();
+  pass
+
+
