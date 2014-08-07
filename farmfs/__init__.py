114ae2b1966e2e9c7fee22a83f10caa5433541e9
Andrew Thomson <athomsonguy@gmail.com>
2014-08-07 00:18:40 -0700

    Add mkfs verb.

--- farmfs/__init__.py
+++ farmfs/__init__.py
@@ -1,4 +1,34 @@
+from fs import ensure_dir
+from os import sep
+
+###########
+# HELPERS #
+###########
+
+def userdata_path(root):
+  return root + sep + ".userdata"
+
+def metadata_path(root):
+  return root + sep + ".metadata"
+
+def keys_path(root):
+  return metadata_path(root) + sep + "keys"
+
+###########
+# OBJECTS #
+###########
 
 class VolumeContext:
   pass
+
+#########
+# VERBS #
+#########
+
+def mkfs(args):
+  ensure_dir(args.root)
+  ensure_dir(userdata_path(args.root))
+  ensure_dir(metadata_path(args.root))
+  ensure_dir(keys_path(args.root))
+  print "FileSystem Created!"
 
