0407eeb176e466ce3a03fc5697441cf06f508bf1
Andrew Thomson <athomsonguy@gmail.com>
2014-08-07 22:54:15 -0700

    Add a layer of indirection for keydb.

--- farmfs/keydb.py
+++ farmfs/keydb.py
@@ -1,10 +1,10 @@
 from os import sep
 from hashlib import md5
-from json import dump, load
+from json import dump, load, JSONEncoder, JSONDecoder
 from errno import ENOENT as NoSuchFile
 
-def checksum(value):
-  return md5(value).hexdigest()
+def checksum(value_str):
+  return md5(str(value_str)).hexdigest()
 
 def key_path(db_path, key):
   return db_path + sep + key
@@ -12,11 +12,14 @@ def key_path(db_path, key):
 class KeyDB:
   def __init__(self, db_path):
     self.root = db_path
+    self.enc = JSONEncoder()
+    self.dec = JSONDecoder()
 
   def write(self, key, value):
-    value_hash = checksum(value)
+    value_str = self.enc.encode(value)
+    value_hash = checksum(value_str)
     obj = {}
-    obj['value'] = value
+    obj['value'] = value_str
     obj['checksum'] = value_hash
     with open(key_path(self.root, key), 'w') as f:
       dump(obj, f)
@@ -25,9 +28,10 @@ class KeyDB:
     try:
       with open(key_path(self.root, key), 'r') as f:
         obj = load(f)
-      value = obj['value']
+      value_str = obj['value']
       value_hash = obj['checksum']
-      assert(checksum(value) == value_hash)
+      assert(checksum(value_str) == value_hash)
+      value = self.dec.decode(value_str)
       return value
     except IOError as e:
       if e.errno == NoSuchFile:
