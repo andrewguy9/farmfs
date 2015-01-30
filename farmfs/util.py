bd335f73bce0beb4d71339d3a000c81e6c4ce8ff
Andrew Thomson <athomsonguy@gmail.com>
2015-01-30 00:52:44 -0800

    Use pytest.

--- farmfs/util.py
+++ farmfs/util.py
@@ -1,6 +1,18 @@
+import pytest
 
+""""
+If zero length array is passed, returns ["."].
+Otherwise returns the origional array.
+Useful for dealing with lists of files, or not.
+"""
 def empty2dot(paths):
   if len(paths) == 0:
     return ["."]
   else:
     return paths
+
+def test_empty2dot():
+  assert empty2dot([]) == ["."]
+  l = [1,2,3]
+  assert empty2dot(l) == l
+
