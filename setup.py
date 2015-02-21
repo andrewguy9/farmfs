aebbf6c9caf2a2ec5ba68397fabf58258fb1d770
Jenny Chen <jenny@wanderable.com>
2015-02-21 10:10:59 -0800

    i fix another typo. andrew yelling.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='farmfs',
       author_email='athomsonguy@gmail.com',
       license='MIT',
       packages=['farmfs'],
-      install_requires = ['prototypes'],
+      install_requires = ['prototype'],
       entry_points = {
         'console_scripts': [
           'farmfs = farmfs.farmui:main',
