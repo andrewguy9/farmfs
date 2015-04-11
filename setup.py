45dd321ae3326c1805ee04f9cccf4fe841117a20
Andrew Thomson <athomsonguy@gmail.com>
2015-04-11 12:29:39 -0700

    fix version and deps in setup.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 tests_require = ['tox', 'pytest']
 
 setup(name='farmfs',
-      version='0.1',
+      version='0.1.2',
       description='tool which de-duplicates files in a filesystem by checksum.',
       url='http://github.com/andrewguy9/farmfs',
       author='andrew thomson',
       author_email='athomsonguy@gmail.com',
       license='MIT',
       packages=['farmfs'],
-      install_requires = ['func_prototypes'],
+      install_requires = ['func_prototypes', 'docopt'],
       tests_require=tests_require,
       extras_require={'test': tests_require},
       entry_points = {
