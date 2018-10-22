a3e779cb51734dd2da7437d359a0044091864678
Andrew Thomson <athomsonguy@gmail.com>
2018-10-22 13:25:03 -0700

    Fix access of private field.

--- farmfs/ui.py
+++ farmfs/ui.py
@@ -77,10 +77,10 @@ def main():
       def print_missing_blob(csum, items):
         print "CORRUPTION missing blob %s" % csum
         for item in items:
-          #TODO _path is private.
           #TODO [1:] is a hack!
           #TODO would like to have snap name.
-          path = Path(item._path[1:], vol.root)
+          props = item.get_dict()
+          path = Path(props['path'][:1], vol.root)
           print "\t%s"%path.relative_to(cwd, leading_sep=False)
       def print_checksum_mismatch(csum):
         print "CORRUPTION checksum mismatch in blob %s" % csum
