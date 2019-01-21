bd27e372d96151facaabf633e47dcdc98e6ee1b7
Andrew Thomson <athomsonguy@gmail.com>
2019-01-21 11:47:19 -0800

    Add tests for userPath2Path.

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,4 +1,6 @@
 from farmfs.fs import normpath as _normalize
+from farmfs.fs import userPath2Path as up2p
+from farmfs.fs import Path
 
 def test_normalize_abs():
   assert _normalize("/")       == "/"
@@ -17,4 +19,7 @@ def test_normalize_relative():
   assert _normalize("a//b")   == "a/b"
   assert _normalize("a//b//") == "a/b"
 
+def test_userPath2Path():
+  assert up2p("c", Path("/a/b")) == Path("/a/b/c")
+  assert up2p("/c", Path("/a/b")) == Path("/c")
 
