7b8e3e283d5cec24086630946bfd04d01f35b7e9
Andrew Thomson <athomsonguy@gmail.com>
2020-04-04 16:54:04 -0700

    parameterize the freeze thaw test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -14,37 +14,42 @@ def test_farmfs_mkfs(tmp_path):
     keys = Path("keys", meta)
     assert keys.isdir()
 
-def test_farmfs_freeze_thaw(tmp_path):
+@pytest.mark.parametrize(
+    "parent,child",
+    [
+        ("a", "b"),
+        ],)
+def test_farmfs_freeze_thaw(tmp_path, parent, child):
     root = Path(str(tmp_path))
     r1 = farmfs_ui(['mkfs'], root)
     assert r1 == 0
-    a = Path('a', root)
-    ab = Path('a/b', root)
-    a.mkdir()
-    with ab.open('w') as bfd:
-        bfd.write("hi")
-    assert a.isdir()
-    assert ab.isfile()
+    parent_path = Path(parent, root)
+    child_path = Path(child, parent_path)
+    parent_path.mkdir()
+    with child_path.open('w') as child_fd:
+        child_fd.write("hi")
+    assert parent_path.isdir()
+    assert child_path.isfile()
     r2 = farmfs_ui(['freeze'], root)
     assert r2 == 0
-    assert a.isdir()
-    assert ab.islink()
-    hi_blob = ab.readlink()
-    assert hi_blob.isfile()
+    assert parent_path.isdir()
+    assert child_path.islink()
+    blob = child_path.readlink()
+    assert blob.isfile()
     userdata = Path('.farmfs/userdata', root)
-    assert userdata in list(hi_blob.parents())
-    with hi_blob.open('r') as hifd:
-        content = hifd.read()
+    assert userdata in list(blob.parents())
+    with blob.open('r') as check_fd:
+        content = check_fd.read()
     assert content == "hi"
     r3 = farmfs_ui(['snap', 'make', 'mysnap'], root)
     assert r3 == 0
     snap = root.join(".farmfs/snap/mysnap")
     snap.exists()
-    ab.unlink()
-    assert not ab.exists()
-    assert hi_blob.isfile()
+    child_path.unlink()
+    assert not child_path.exists()
+    assert blob.isfile()
     r4 = farmfs_ui(['snap', 'restore', 'mysnap'], root)
     assert r4 == 0
-    assert ab.islink()
-    assert hi_blob.isfile()
-    assert ab.readlink() == hi_blob
+    assert child_path.islink()
+    assert blob.isfile()
+    assert child_path.readlink() == blob
