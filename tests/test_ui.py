92aed575a975468cff148a90634da60cff7a2eef
Andrew Thomson <athomsonguy@gmail.com>
2020-04-04 16:35:54 -0700

    add snap/restore to ui test.

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -16,7 +16,8 @@ def test_farmfs_mkfs(tmp_path):
 
 def test_farmfs_freeze_thaw(tmp_path):
     root = Path(str(tmp_path))
-    farmfs_ui(['mkfs'], root)
+    r1 = farmfs_ui(['mkfs'], root)
+    assert r1 == 0
     a = Path('a', root)
     ab = Path('a/b', root)
     a.mkdir()
@@ -24,12 +25,26 @@ def test_farmfs_freeze_thaw(tmp_path):
         bfd.write("hi")
     assert a.isdir()
     assert ab.isfile()
-    farmfs_ui(['freeze'], root)
+    r2 = farmfs_ui(['freeze'], root)
+    assert r2 == 0
     assert a.isdir()
     assert ab.islink()
     hi_blob = ab.readlink()
+    assert hi_blob.isfile()
     userdata = Path('.farmfs/userdata', root)
     assert userdata in list(hi_blob.parents())
     with hi_blob.open('r') as hifd:
         content = hifd.read()
     assert content == "hi"
+    r3 = farmfs_ui(['snap', 'make', 'mysnap'], root)
+    assert r3 == 0
+    snap = root.join(".farmfs/snap/mysnap")
+    snap.exists()
+    ab.unlink()
+    assert not ab.exists()
+    assert hi_blob.isfile()
+    r4 = farmfs_ui(['snap', 'restore', 'mysnap'], root)
+    assert r4 == 0
+    assert ab.islink()
+    assert hi_blob.isfile()
+    assert ab.readlink() == hi_blob
