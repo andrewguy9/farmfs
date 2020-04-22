88614ab4b884304ae97975d89982e15680b481c9
Andrew Thomson <athomsonguy@gmail.com>
2020-04-21 17:07:10 -0700

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requires = ['tox', 'pytest==4.6.8', 'tabulate']
 
 setup(
     name='farmfs',
-    version='0.7.4',
+    version='0.7.5',
     author='Andrew Thomson',
     author_email='athomsonguy@gmail.com',
     packages=['farmfs'],
